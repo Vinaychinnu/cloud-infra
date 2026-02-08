a8281b6e9752b7ca458e3b9333919971417ad269
Vinaychinnu <kombathulavinaykumar@gmail.com>
2026-02-08 13:05:15 +0530

    feat: add internet gateway and public route table

--- modules/vpc/main..tf
+++ modules/vpc/main..tf
@@ -20,3 +20,31 @@ resource "aws_subnet" "public" {
     Name = "${var.name}-public-${count.index + 1}"
   }
 }
+
+resource "aws_internet_gateway" "this" {
+  vpc_id = aws_vpc.this.id
+
+  tags = {
+    Name = "${var.name}-igw"
+  }
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.this.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.this.id
+  }
+
+  tags = {
+    Name = "${var.name}-public-rt"
+  }
+}
+
+resource "aws_route_table_association" "public" {
+  count = length(aws_subnet.public)
+
+  subnet_id      = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.public.id
+}
